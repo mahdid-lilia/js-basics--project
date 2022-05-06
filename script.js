d4db333082b38bd161d5093ff8b2b65ea2baf8c2
thisisMoe <bounoua.smm@gmail.com>
2022-05-07 00:46:27 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Prepare quiz (questions, answers and correct answer)
+// Show first question from the quiz when first loading the page
+// Show first question from the quiz when first loading the page
+    // 1 . Grab all related html elements (question text, radio buttons, answer texts, submit btn, quiz container )
+    // 2. track which question we are at and change content accordingly (loadQuiz)
+// When submitting, grab the selected answer, add to score if correct, increment current quiz and uncheck all radio buttons.
+// Check if end of quiz, if true show score, if not true load next quiz
+// reload page when finished
